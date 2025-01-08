0978c2f0fbe5cf05d693732d299a96f952770984
Tasneem Ali <tasneemali_@outlook.com>
2025-01-08 15:23:43 -0500

    Add files via upload
    This program was made to practice my skills in creating and reusing variables in Python. Take a look!

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator!")
+user_city = input("What's the name of the city you grew up in?\n")
+user_pet = input("What's the name of your pet?\n")
+print("Your band name could be " + user_city + " " + user_pet + ". How rad!")
