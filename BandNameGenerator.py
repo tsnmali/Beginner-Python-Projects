c0bd39c2febccf6cf7d047bda8c115724baf12b9
Tasneem Ali <tasneemali_@outlook.com>
2025-01-13 15:07:40 -0500

    ClubNameGenerator.py
    A program that generates a unique name for your band!
    
    The purpose of this program is to:
    
    - Use string concatenation
    - Utilize variables to store data
    - Use 'print' statements

--- BandNameGenerator.py
+++ BandNameGenerator.py
@@ -1,4 +1,11 @@
-print("Welcome to Band Name Generator!")
+# A simple program to generate a unique club name.
+print("Welcome to Club Name Generator!")
+
+# Ask the user for the city they grw up in and storing the response in a variable.
 user_city = input("What's the name of the city you grew up in?\n")
+
+# As the user for their pet and storing the response in a variable.
 user_pet = input("What's the name of your pet?\n")
+
+# Using string concatenation to combine the user's city and pet name to create a unique club name!
 print("Your band name could be " + user_city + " " + user_pet + ". How rad!")
