033cfaa26930ac607a27342c7344638800cda85a
Tasneem Ali <tasneemali_@outlook.com>
2025-01-13 15:10:17 -0500

    Update ClubNameGenerator.py

--- ClubNameGenerator.py
+++ ClubNameGenerator.py
@@ -4,7 +4,7 @@ print("Welcome to Club Name Generator!")
 # Ask the user for the city they grw up in and storing the response in a variable.
 user_city = input("What's the name of the city you grew up in?\n")
 
-# As the user for their pet and storing the response in a variable.
+# Ask the user for their pet and storing the response in a variable.
 user_pet = input("What's the name of your pet?\n")
 
 # Using string concatenation to combine the user's city and pet name to create a unique club name!
